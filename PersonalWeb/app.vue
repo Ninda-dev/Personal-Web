4a068de67de394a44ef334d5e265c2eeab3a80a7
Ninda-dev <armilaninda@gmail.com>
2024-12-09 15:38:52 +0700

    Component test running

--- PersonalWeb/app.vue
+++ PersonalWeb/app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import Navbar from './src/components/Navbar.vue';
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <Navbar />
     Halo dunia
     <NuxtWelcome />
   </div>
---
+++ PersonalWeb/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const testName = 'Tasya';
+
+    const showName = () => {
+      console.log(testName);
+    }
+</script>
+
+<template>
+  <div>
+    <h1>Navbar</h1>
+    <p>Nama : {{ testName }}</p>
+    <button @click="showName">Show Name in inspect</button>
+  </div>
+</template>
+
+<style>
+
+</style>
